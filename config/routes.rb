3344a6aa56b6a99d9a81c481913b068180924631
kenjirou-shinsui <kenjirou.shinsui.0221@gmail.com>
2020-01-08 03:43:19 +0000

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'main_accounts/index'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :public do
+    get 'deli_addresses/index'
+    get 'deli_addresses/edit'
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
