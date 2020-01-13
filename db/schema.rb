cd9391ec7325aa5703d837d2f74e7ae09d659c7f
mameg725 <mameg.725@gmail.com>
2020-01-13 10:03:47 +0000

    add order_product/quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_12_052035) do
-
+ActiveRecord::Schema.define(version: 2020_01_13_071944) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_052035) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.datetime "deleted_at"
   end
 
   create_table "deli_addresses", force: :cascade do |t|
@@ -46,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_052035) do
     t.integer "tel_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.datetime "deleted_at"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -73,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_052035) do
     t.text "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|
