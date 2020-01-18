fe876939429e9373b7b76402f2e3e6416d4f72fd
mameg725 <mameg.725@gmail.com>
2020-01-18 04:37:34 +0000

    order_product/show

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
 	belongs_to :user
 	has_many :order_products, dependent: :destroy
+  accepts_nested_attributes_for :order_products
 	has_many :deli_address
 
     composed_of :fullname,
