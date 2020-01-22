6141aab8ecbc57b44ba35b7360f540b6202c4330
kenjirou-shinsui <kenjirou.shinsui.0221@gmail.com>
2020-01-22 03:47:44 +0000

    updated file

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,4 +1,4 @@
 class Public::ApplicationController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show,:genreshow]
 
 end
